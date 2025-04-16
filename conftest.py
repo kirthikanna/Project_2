d4bd731c88dd64466e67f8ff0005249c6d01241c
kirthikanna <150948986+kirthikanna@users.noreply.github.com>
2025-04-16 22:03:59 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -15,3 +18,12 @@ def setup():
     yield driver
     driver.quit()
     print("\nBrowser closed")
+
+@pytest.fixture(scope="function")
+def login(setup):
+    driver = setup
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.visibility_of_element_located((By.NAME, "username"))).send_keys("Admin")
+    wait.until(EC.visibility_of_element_located((By.NAME, "password"))).send_keys("admin123")
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+    return driver
